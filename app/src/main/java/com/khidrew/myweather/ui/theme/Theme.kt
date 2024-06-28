8b316bb4836dbede9b12de16c91cf1505874e7bf
xontelAndroid <mohamed.khidr@xontel.com>
2024-06-28 04:15:18 +0300

    Initial commit

---
+++ app/src/main/java/com/khidrew/myweather/ui/theme/Theme.kt
@@ -0,0 +1,47 @@
+package com.khidrew.myweather.ui.theme
+
+import android.app.Activity
+import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+
+private val DarkColorScheme = darkColorScheme(
+    primary = Dark200,
+    secondary = Dark500,
+)
+
+private val LightColorScheme = lightColorScheme(
+    primary = Dark200,
+    secondary = Dark500,
+)
+
+@Composable
+fun MyWeatherTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    // Dynamic color is available on Android 12+
+    dynamicColor: Boolean = false,
+    content: @Composable () -> Unit
+) {
+    val colorScheme = when {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
+
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
+    }
+    rememberSystemUiController().isSystemBarsVisible = false
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
+    )
+}
