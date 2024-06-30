fcec01a382f5947805a1c3c18e56c9e344e22930
xontelAndroid <mohamed.khidr@xontel.com>
2024-07-01 02:11:02 +0300

    Daily forecast

--- app/src/main/java/com/khidrew/myweather/screens/WeatherScreen.kt
+++ app/src/main/java/com/khidrew/myweather/screens/WeatherScreen.kt
@@ -13,11 +13,14 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.filled.ArrowDownward
+import androidx.compose.material.icons.filled.ArrowUpward
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -120,6 +123,7 @@ viewModel: WeatherViewModel = viewModel()
                 items(data.data) { forecast ->
                     Column(
                         modifier = Modifier
+                            .padding(vertical = 4.dp)
                             .size(100.dp, 140.dp)
                             .clip(RoundedCornerShape(16.dp))
                             .background(MaterialTheme.colorScheme.secondary),
@@ -127,24 +131,97 @@ viewModel: WeatherViewModel = viewModel()
                         horizontalAlignment = Alignment.CenterHorizontally
                     ) {
                         Text(
+                            modifier = Modifier.weight(1f),
                             text = SimpleDateFormat("H a").format(Date(forecast.epochDataTime * 1000)),
                             color = Color.Gray
                         )
-                        Spacer(modifier = Modifier.height(8.dp))
+                        Spacer(modifier = Modifier.height(4.dp))
                         AsyncImage(
+                            modifier = Modifier.weight(3f),
                             model = ImageRequest.Builder(
                                 LocalContext.current
                             )
                                 .data("https://developer.accuweather.com/sites/default/files/${forecast.weatherIcon.fixIcon()}-s.png")
                                 .build(),
-                            contentScale = ContentScale.Fit,
+                            contentScale = ContentScale.FillWidth,
                             contentDescription = null,
                         )
-                        Spacer(modifier = Modifier.height(8.dp))
-                        Text(text = forecast.temperature.value.toString(), color = Color.White)
+                        Spacer(modifier = Modifier.height(4.dp))
+                        Text(
+                            modifier = Modifier.weight(1f),
+                            text = forecast.temperature.value.toString(),
+                            color = Color.White
+                        )
                     }
                 }
             }
+        }
+        AnimatedVisibility(visible = hourlyForecasts is Result.Loading) {
+            Loading()
+        }
+        Spacer(modifier = Modifier.height(10.dp))
+        Text(
+            text = "Daily forecasts",
+            fontWeight = FontWeight.Bold,
+            fontSize = 18.sp,
+            color = Color.White
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        AnimatedVisibility(visible = dailyForecasts is Result.Success) {
+            val data = dailyForecasts as Result.Success
+            LazyColumn(
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                items(data.data.dailyForecasts) { forecast ->
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(56.dp)
+                            .clip(RoundedCornerShape(4.dp))
+                            .background(MaterialTheme.colorScheme.secondary)
+                            .padding(start = 16.dp, end = 8.dp),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text(
+                            text = "${SimpleDateFormat("d").format(Date(forecast.epochDate * 1000))}",
+                            color = Color.White
+                        )
+                        Row {
+                            Icon(
+                                Icons.Default.ArrowDownward,
+                                tint = Color(0xffff5353),
+                                contentDescription = null
+                            )
+                            Text(
+                                text = "${forecast.temperature.minimum.value}°",
+                                color = Color.White
+                            )
+                            Spacer(modifier = Modifier.height(6.dp))
+                            Icon(
+                                Icons.Default.ArrowUpward,
+                                tint = Color(0xff2eff8c),
+                                contentDescription = null
+                            )
+                            Text(
+                                text = "${forecast.temperature.maximum.value}°",
+                                color = Color.White
+                            )
+                        }
+
+                        AsyncImage(
+                            modifier = Modifier.size(70.dp),
+                            model = ImageRequest.Builder(LocalContext.current)
+                                .data("https://developer.accuweather.com/sites/default/files/${forecast.day.icon.fixIcon()}-s.png").build(),
+                            contentDescription = null,
+                            contentScale = ContentScale.Fit
+                        )
+                    }
+                }
+            }
+        }
+        AnimatedVisibility(visible = dailyForecasts is Result.Loading) {
+            Loading()
         }
 
 
